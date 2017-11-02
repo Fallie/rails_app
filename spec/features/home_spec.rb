8ae31525231ae86efb437a9c7a10e3629133d8ed
Fallie Zhang <fallie@cnsdose.com>
2017-11-02 23:24:30 +1100

    Fix feature test and add scss style.

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,7 +4,7 @@ describe "Home" do
 	describe "GET /home" do
 		it "displays the welcome" do
 			visit static_views_home_path
-			page.should have_content 'Welcome user!'
+			page.should have_content 'Welcome to the Rails App'
 		end
 	end
 end
