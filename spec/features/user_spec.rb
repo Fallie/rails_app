ad36d81b147db21f029c74228fb3a18f28f21b94
Fallie Zhang <fallie@cnsdose.com>
2017-11-04 23:24:51 +1100

    Fix feature test.

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,20 +2,10 @@ require 'rails_helper'
 
 describe "User" do
 	describe "GET /signup" do
-		let(:some_instance) { double(Class_name) }
-		let(:results) do
-
-		end
-
-		before do
-			allow(some_instance).to receive(:method_name).with().and_return(results)
-		end
 
 		it "displays the signup form" do
 			visit sign_up_path
 			page.should have_content 'Sign up'
-
-			expect(bklwqaw).to  
 		end
 	end
 end
